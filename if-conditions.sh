170495787e76d1078d693c6cf82119e2ec9da085
balamuralich <jyobalach@gmail.com>
2025-10-01 21:10:07 +0530

    shell

--- if-conditions.sh
+++ if-conditions.sh
@@ -5,13 +5,15 @@ g="\e[32m"
 y="\e[33m"
 n="\e[0m"
 
-echo "enter u r age " 
-
-read age
+echo "enter your name " 
 read name
+echo "please enter your age"
+read age
 
 if [ $age -lt 21 ]; then
     echo -e "$name is $r not eligible $n to vote"
+    echo -e "age should be greater than 21"
+
 else 
     echo -e "$name is $g eligible $n to vote"
 fi
