f9dcf5338dda357f912ea431595f93856c54f70d
balamuralich <jyobalach@gmail.com>
2025-10-01 20:38:55 +0530

    shell

--- shell-roboshop.sh
+++ shell-roboshop.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+r="\e[31m"
+g="\e[32m"
+y="\e[33m"
+n="\e[0m"
+
+
 member1=bala
 member2=jyo
 
-echo "hey $member1 did you eat dinner, asked by $member2"
-echo "hi $member2 yes"
+echo -e "hey $g $member1 $n did you eat dinner, asked by $member2"
+echo -e "hi $g $member2 $n $y yes $n"
 
